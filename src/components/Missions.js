f5bd4a5125d0a0f49ae9925c9ce0f87b03f09e90
Leonardo Zanette <leonardobzanette@gmail.com>
2022-06-21 16:56:11 -0300

    Requisito 7 - DONE

---
+++ src/components/Missions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Title from './Title';
+// import planets from '../data/planets';
+// import PlanetCard from './PlanetCard';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+
+export default Missions;
