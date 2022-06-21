9a15997371d3a9d09f71ddd6c93db20449f9f5bc
Leonardo Zanette <leonardobzanette@gmail.com>
2022-06-21 15:57:38 -0300

    Requisito 3 - DONE

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,10 @@
 import React from 'react';
+// import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <h1>Sistema Solar</h1>
-      </div>
+      <div data-testid="solar-system" />
     );
   }
 }
